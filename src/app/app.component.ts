b1afc7be2a7825b3152e7c25b66f03b342067369
rafael <rafaelljj@gmail.com>
2025-06-10 16:06:38 -0300

    :sparkles: feat: Commit inicial - Listagem de itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HelloworldComponent } from './helloworld/helloworld/helloworld.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CalculadoraComponent],
+  imports: [RouterOutlet, CalculadoraComponent, ListaComprasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
